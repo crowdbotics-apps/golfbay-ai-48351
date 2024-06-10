9acdfef3dd4d72f9bcc787e19184df11023415d1
Crowdbotics <team@crowdbotics.com>
2024-06-10 23:49:17 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "golfbay-ai-48351",
+  url: "https://golfbay-ai-48351.botics.co",
+  api: "https://golfbay-ai-48351.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
